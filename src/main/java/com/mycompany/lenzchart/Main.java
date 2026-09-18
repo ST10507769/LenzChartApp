b6f7da0295aa79c3d707da8f561a68e35c086869
LenzSt10507769 <ST10507769@rcconnect.edu.za>
2026-09-18 02:36:06 +0200

    Update Main.java
    Added Main Class

--- src/main/java/com/mycompany/lenzchart/Main.java
+++ src/main/java/com/mycompany/lenzchart/Main.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+// Main class
 package com.mycompany.lenzchart;
 
 import java.util.Scanner;
